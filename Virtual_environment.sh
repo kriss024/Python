fbb1e2e26c2502e5ea61e3c5060b8da5429e846f
Krzysztof Bruszewski <krzysztof.bruszewski@gmail.com>
2024-11-05 08:20:59 +0100

    Update Virtual_environment.sh

--- Virtual_environment.sh
+++ Virtual_environment.sh
@@ -44,7 +44,7 @@ pip index versions <package name>
 
 # Install packages from requirements.txt
 python3 -m venv .venv
-source .venv/bin/activate
+source ./venv/bin/activate
 python3 -m pip install --upgrade pip
 pip install -U setuptools wheel
 pip install -r requirements.txt
