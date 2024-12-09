db3201db7c4cb0ddca9b3e1ca55793bac68aad79
Krzysztof Bruszewski <krzysztof.bruszewski@gmail.com>
2024-12-09 18:07:43 +0100

    Update Virtual_environment.sh

--- Virtual_environment.sh
+++ Virtual_environment.sh
@@ -51,11 +51,7 @@ pip install -r requirements.txt
 deactivate
 
 # Install Python Data Science Packages
-# for Windows
-pip install requests six scipy numpy pandas scikit-learn openpyxl statsmodels seaborn matplotlib ipython tqdm
-
-# for Linux
-pip install requests cachetools six python-dateutil scipy numpy pandas scikit-learn scikit-image scikit-plot openpyxl statsmodels pyarrow tables seaborn matplotlib bokeh pydotplus ipython tqdm
+pip install requests cachetools six python-dateutil scipy numpy pandas scikit-learn scikit-image yellowbrick openpyxl statsmodels pyarrow tables seaborn matplotlib bokeh pydotplus ipython tqdm
 # In Linux: pip install xlrd
 pip install plotly kaleido
 pip install 'polars[all]'
