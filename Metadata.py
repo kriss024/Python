810c9a643a622f720cee812eed037c43b06df390
Krzysztof Bruszewski <krzysztof.bruszewski@gmail.com>
2026-03-27 18:30:24 +0100

    Update Metadata.py

--- Metadata.py
+++ Metadata.py
@@ -4,22 +4,25 @@ import pandas as pd
 test_dataframe = pd.DataFrame({'float64': [1.0, 3.0, 4, -5, 6],
                     'float64_null': [1.0, -3.0, np.nan, np.inf, None],
                     'int64': [1, 2, 3, -5, 6],
-                    'int64_null_const': [1, -2, np.nan, np.inf, None],
-                    'int64_const': [1, 1, 1, 1, 1], 
-                    'int64_null_const': [1, 1, np.nan, np.inf, None], 
+                    'int64_null': [1, 2, np.nan, np.inf, None],
+                    'int64_null_const': [1, 1, np.nan, np.inf, None],
+                    'int64_const': [1, 1, 1, 1, 1],
+                    'long_const': [65536*65536, 65536*65536, 65536*65536, 65536*65536, 65536*65536],
+                    'long_null_const': [65536*65536, 65536*65536, np.nan, np.inf, None],
                     'bool': [True, False, True, False, True],
                     'bool_null': [True, False, np.nan, np.inf, None],
-                    'datetime64': [pd.Timestamp('20180310'), pd.Timestamp('20190310'), pd.Timestamp('20200310'), 
+                    'datetime64': [pd.Timestamp('20180310'), pd.Timestamp('20190310'), pd.Timestamp('20200310'),
                                    pd.Timestamp('20210310'), pd.Timestamp('20220310')],
-                     'datetime64_time': [pd.Timestamp('20180310 13:00:15'), pd.Timestamp('20190310 13:00:15'), 
-                                         pd.Timestamp('20200310 13:00:15'), pd.Timestamp('20210310 12:00'), 
+                     'datetime64_time': [pd.Timestamp('20180310 13:00:15'), pd.Timestamp('20190310 13:00:15'),
+                                         pd.Timestamp('20200310 13:00:15'), pd.Timestamp('20210310 12:00'),
                                          pd.Timestamp('20220310 12:00')],
                     'datetime64_null': [pd.Timestamp('20180310'), pd.Timestamp('20190310'), np.nan, np.nan, None],
-                    'object': ['foo','buzz', 'buzz','buzz', 'buzz'], 
-                    'object_null': ['foo','buzz', np.nan, np.inf, None], 
-                    'nans_inf_nones': [np.nan, np.inf, None, np.nan, None], 
+                    'object': ['foo','buzz', 'buzz','buzz', 'buzz'],
+                    'object_null': ['foo','buzz', np.nan, np.inf, None],
+                    'object_const_null': ['foo','foo', None, None, None],
+                    'nans_inf_nones': [np.nan, np.inf, None, np.nan, None],
                     'nans_nones': [np.nan, np.nan, None, np.nan, None],
-                    'nones': [None, None, None, None, None]
+                    'nulls': [None, None, None, None, None]
                    })
 
 catCol = np.array(list('ABCD'))[np.random.randint(4, size=100)]
@@ -40,7 +43,7 @@ test_dataframe2 = pd.DataFrame({
     'numCol': numCol,
     'datCol': datCol,
     'boolCol': boolCol,
-    'constCol_longlonglonglonglonglongName': constCol
+    'constCol': constCol
 })
 
 def remove_all_by_values(list_obj, values):
@@ -55,7 +58,7 @@ def dataframe_metadata(data):
     if isinstance(data, pd.DataFrame):
 
         master = pd.DataFrame()
-      
+
         for column in data.columns:
             datatype = str(data.dtypes[column])
             total = data.shape[0]
@@ -76,15 +79,15 @@ def dataframe_metadata(data):
             data_list = list()
             for index in range(0, min_elem):
                 data_list.append(val_list[index])
-            
+
             val_str = str(data_list)
 
-            data_dict = {'name': [column], 'type': [datatype], 
+            data_dict = {'name': [column], 'type': [datatype],
                         'total': [total], 'non_null': [non_null], 'null': [null], 'proc_null': [proc_null],
                         'unique': [unique],
                         'data': val_str
                         }
-                        
+
             row = pd.DataFrame.from_dict(data_dict)
             master = pd.concat([master, row])
 
@@ -110,7 +113,7 @@ def calculate_woe_iv(dataset, feature, target):
                 'Good': dataset[(dataset[feature] == value) & (dataset[target] == 0)].shape[0],
                 'Bad': dataset[(dataset[feature] == value) & (dataset[target] == 1)].shape[0]
             })
-        
+
         dset = pd.DataFrame(lst)
         dset['Share_Total'] = dset['Total'] / dset['Total'].sum()
         dset['Share_Good'] = dset['Good'] / dset['Total']
@@ -121,9 +124,9 @@ def calculate_woe_iv(dataset, feature, target):
         dset = dset.replace({'WoE': {np.inf: 0, -np.inf: 0}})
         dset['IV'] = (dset['Distr_Good'] - dset['Distr_Bad']) * dset['WoE']
         iv = dset['IV'].sum()
-        
+
         dset = dset.sort_values(by='WoE')
-        
+
         return dset, iv
 
     else:
